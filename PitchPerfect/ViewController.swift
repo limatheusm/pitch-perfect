a65c4712e1f273b32b0416f2ce179d12c4ba27d5
Matheus Lima <matheus.lima@lavid.ufpb.br>
2018-08-26 11:12:37 -0300

    Initial commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Matheus Lima on 26/08/18.
+//  Copyright © 2018 Matheus Lima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
